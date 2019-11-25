f47b5d19f709a5ed6fbbff13a16a4e1df62f10e8
Giovanni Gobbi <giovannigobbibusiness@gmail.com>
2019-11-24 17:43:37 -0800

    Add initial version of index view and listing

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,34 @@
 import { checkSession } from "/public/js/session.mjs"
 
+function updateData(data) {
+    data = JSON.parse(data);
+    var wishlist = document.getElementById("wishlist");
+    var items = "";
+    data["wishItems"].forEach(element => {
+        items += `<li>${element["item"]}</li>`;
+    });
+    if(items == "") {
+        wishlist.innerText = "No items currently listed";
+    } else {
+        wishlist.innerHTML = items;
+    }
+}
+
+function getSessionData(access_token) {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
+            updateData(xhr.responseText);
+        }
+    };
+    xhr.open("GET", `http://fa19server.appspot.com/api/wishlists/myWishlist?access_token=${access_token}`, true);
+    xhr.send();
+}
 
 window.addEventListener("DOMContentLoaded", () => {
     var access_token = checkSession();
     if(access_token == undefined) {
         window.location.href = "/public/login.html";
     }
+    getSessionData(access_token);
 });
